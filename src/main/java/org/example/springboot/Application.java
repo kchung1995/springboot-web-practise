d0f7297b8b19a6cfa6c55c07b77f070aff845e59
KuyHo Chung <kchung1995@gmail.com>
2021-07-09 03:44:17 +0900

    added teset codes, API codes and basetime entity

--- src/main/java/org/example/springboot/Application.java
+++ src/main/java/org/example/springboot/Application.java
@@ -4,6 +4,9 @@ package org.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
 
 //@SpringBootApplication으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성이 모두 자동으로 설정된다.
 //@SpringBootApplication이 있는 위치부터 설정을 읽어가므로, 이 클라스는 항상 프로젝트의 최상단에 위치해야 한다.
